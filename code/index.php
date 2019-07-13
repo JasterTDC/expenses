a6940984342594a9ddd31e8647c3dd46d35f28aa
Ismael Moral <imoral@lenovo.Home>
2019-07-13 17:56:48 +0200

    Initial structure
    We add the basic for a slim project.

--- code/index.php
+++ code/index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo phpinfo();
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Slim\App;
+
+ini_set('display_errors', true);
+
+error_reporting(E_ALL);
+
+$application = new App();
+
+$container = $application->getContainer();
+
+$application->run();
