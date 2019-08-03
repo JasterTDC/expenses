79389e3170eb38d3ed378e1efd6a63539389726f
Ismael Moral <jastertdc@gmail.com>
2019-08-03 17:57:06 +0200

    feature/logout
    Se añade funcionalidad para logout de un usuario.

---
+++ code/src/Infrastructure/Controller/User/LogoutController.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace ComAI\Expenses\Infrastructure\Controller\User;
+
+use ComAI\Expenses\Domain\ValueObject\UserLoginCookie;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+/**
+ * Class LogoutController
+ *
+ * @package ComAI\Expenses\Infrastructure\Controller\User
+ * @author  Ismael Moral <jastertdc@gmail.com>
+ */
+class LogoutController
+{
+
+    /**
+     * LogoutController constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     *
+     * @return Response
+     */
+    public function __invoke(
+        Request $request,
+        Response $response
+    ) : Response {
+        if (isset($_COOKIE[UserLoginCookie::LOGIN_COOKIE])) {
+            unset($_COOKIE[UserLoginCookie::LOGIN_COOKIE]);
+
+            setcookie(
+                UserLoginCookie::LOGIN_COOKIE,
+                null,
+                -1,
+                '/'
+            );
+        }
+
+        return $response->withJson([
+            'success' => true
+        ]);
+    }
+}
